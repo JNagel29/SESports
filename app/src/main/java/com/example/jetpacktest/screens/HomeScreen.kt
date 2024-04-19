d5b5da32e233b92a1db0882893f3b5a30f6468f7
niko <>
2024-04-18 23:59:14 -0700

    removed extra imports

--- app/src/main/java/com/example/jetpacktest/screens/HomeScreen.kt
+++ app/src/main/java/com/example/jetpacktest/screens/HomeScreen.kt
@@ -31,7 +31,6 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.example.jetpacktest.R
 import com.example.jetpacktest.models.StatLeader
 import com.example.jetpacktest.ui.components.ExpandableCard
 import com.example.jetpacktest.ui.theme.JetpackTestTheme
--- app/src/main/java/com/example/jetpacktest/ui/components/ExpandableCard.kt
+++ app/src/main/java/com/example/jetpacktest/ui/components/ExpandableCard.kt
@@ -27,7 +27,6 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
